9e9755a90219e3739a73e006d44dabb0c837ac7e
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2014-01-17 20:18:13 -0600

    Update code to make even more images.

--- talks/overview/primer-images.rkt
+++ talks/overview/primer-images.rkt
@@ -1,8 +1,6 @@
 #lang racket
 (require psaifr/main)
 
-; NOTE: I should rewrite this whole thing as an association list.
-
 (define negative-one '-1)
 (define zero '0)
 (define one '1)
@@ -24,26 +22,42 @@
 (define x-times-y '(* x y))
 (define negate-x-times-y '(negate (* x y)))
 (define x-squared '(* x x))
-(define sign-x-times-y '(* (sign x) y))
-(define sign-x-times-sign-y '(* (sign x) (sign y)))
 (define MULTIPLICATION 
   (list 'x-times-half 
         'x-times-quarter 
         'x-times-y 
         'negate-x-times-y
         'x-squared))
+
+(define sign-x-times-half '(sign (* x 0.5)))
+(define sign-x-times-y '(* (sign x) y))
+(define sign-x-times-sign-y '(* (sign x) (sign y)))
+(define half-sign-x '(* 1/2 (sign x)))
+(define SIGN 
+  (list 'sign-x-times-half
+        'sign-x-times-y
+        'sign-x-times-sign-y
+        'half-sign-x))
  
 (define x-plus-half '(add x 0.5))
 (define x-plus-one '(add x 1))
 (define x-minus-half '(add x -0.5))
 (define x-plus-y '(add x y))
 (define x-minus-y '(add x (negate y)))
+(define sign-x-plus-half '(sign (+ x 0.5)))
+(define sign-x-plus-quarter '(sign (+ x 0.25)))
+(define sign-x-minus-half '(sign (+ x -0.5)))
+(define sign-x-plus-sign-y '(+ (sign x) (sign y)))
 (define ADDITION 
   (list 'x-plus-half 
         'x-plus-one
         'x-minus-half 
         'x-plus-y 
-        'x-minus-y))
+        'x-minus-y
+        'sign-x-plus-half
+        'sign-x-plus-quarter
+        'sign-x-minus-half
+        'sign-x-plus-sign-y))
 
 (define x-rotate-one '(rotate x 1))
 (define x-rotate-half '(rotate x 0.5))
@@ -68,8 +82,17 @@
 (define circle1 '(add (* x x) (* y y)))
 (define circle2 '(sign (add (* x x) (* y y))))
 (define circle3 '(sign (add -0.99 (add (* x x) (* y y)))))
+(define half-circle `(* ,sign-x-plus-y ,circle1))
 (define CIRCLES 
-  (list 'circle1 'circle2 'circle3))
+  (list 'circle1 'circle2 'circle3
+        'half-circle))
+
+(define vertical-line-1 '(sign (- (abs x) .05)))
+(define vertical-line-2 '(sign (- (abs (+ x 1/4)) .1)))
+(define diagonal-line-1 '(sign (- (abs (- y x)) .2)))
+(define LINES
+  (list 'vertical-line-1 'vertical-line-2
+        'diagonal-line-1))
 
 (define sine-x '(sine x))
 (define cosine-y '(cosine y))
@@ -78,7 +101,8 @@
         'cosine-y))
 
 (define descriptions
-  (append BASIC MULTIPLICATION ADDITION ROTATION COMPLEX CIRCLES TRIG))
+  (append BASIC MULTIPLICATION SIGN ADDITION ROTATION 
+          COMPLEX CIRCLES LINES TRIG))
 
 (define description->fname
   (lambda (description)
